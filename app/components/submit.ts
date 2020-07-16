89f670feee69a4e86aec5f9730b89c94bce301b0
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2020-07-16 19:21:52 +0200

    2020.07.16 - 2

---
+++ app/components/submit.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface SubmitArgs {}
+
+export default class Submit extends Component<SubmitArgs> {}
---
+++ app/components/submit/button.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface SubmitButtonArgs {}
+
+export default class SubmitButton extends Component<SubmitButtonArgs> {}
